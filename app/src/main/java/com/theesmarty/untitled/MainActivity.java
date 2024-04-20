318ce4d891d07a66ffe2d60f226f5adc36242d8a
The-Smarty <phanipraharsha25@gmail.com>
2024-04-20 22:02:26 +0530

    Added spinner for playercount

--- app/src/main/java/com/theesmarty/untitled/MainActivity.java
+++ app/src/main/java/com/theesmarty/untitled/MainActivity.java
@@ -4,18 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView img;
     private Random r;
+    Spinner sp;
+    TextView player;
+    String[] playerCount = {"2 Players", "3 Players", "4 Players","5 Player"};
+    ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,playerCount);
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         img=findViewById(R.id.dice);
+        sp = (Spinner) findViewById(R.id.players);
+        player = findViewById(R.id.PlayerId);
         img.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
             case 5: img.setImageResource(R.drawable.dice_5);
                 break;
             case 6: img.setImageResource(R.drawable.dice_6);
+                rollDice();
                 break;
         }
     }
