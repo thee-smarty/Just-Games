fd3eea482f06bc79cb15ee6c93c9c1b6ab838d49
The-Smarty <phanipraharsha25@gmail.com>
2024-04-25 21:17:44 +0530

    Update Function for elements

--- app/src/main/java/com/theesmarty/untitled/MainActivity.java
+++ app/src/main/java/com/theesmarty/untitled/MainActivity.java
@@ -1,7 +1,9 @@
 package com.theesmarty.untitled;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -14,9 +16,21 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    CardView g1,g2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        g1 = findViewById(R.id.dice);
+        g2 = findViewById(R.id.rps);
+
+        g1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(MainActivity.this,DiceActivity.class);
+                startActivity(in);
+            }
+        });
     }
 }
