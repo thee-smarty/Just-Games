64f9135d6619be4da51b91f7dfcc4eb47092993b
The-Smarty <phanipraharsha25@gmail.com>
2024-04-27 17:53:39 +0530

    Created activity for RockPaperScissor

---
+++ app/src/main/java/com/theesmarty/untitled/RockPaperScissorActivity.java
@@ -0,0 +1,14 @@
+package com.theesmarty.untitled;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RockPaperScissorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rock_paper_scissor);
+    }
+}
