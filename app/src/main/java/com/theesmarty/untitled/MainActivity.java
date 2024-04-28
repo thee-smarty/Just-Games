3618f18479a0d46778ee85b6293938b8ff678f0e
The-Smarty <phanipraharsha25@gmail.com>
2024-04-28 11:38:18 +0530

    Bug fixes

--- app/src/main/java/com/theesmarty/untitled/MainActivity.java
+++ app/src/main/java/com/theesmarty/untitled/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent in = new Intent(MainActivity.this,RockPaperScissorActivity.class);
                 startActivity(in);
+                finish();
             }
         });
     }
